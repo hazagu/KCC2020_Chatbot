dcbae204fb0f1a54d594e381e4137070d8918eea
tnatna0801 <min120926@gmail.com>
2020-07-25 20:26:54 +0900

    dispatch mannerbot

---
+++ Gate04/Dispatch_testbot/Dispatch_testbot/IBotServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.AI.Luis;
+using Microsoft.Bot.Builder.AI.QnA;
+
+namespace Dispatch_testbot
+{
+    public interface IBotServices
+    {
+        LuisRecognizer Dispatch { get; }
+        QnAMaker SampleQnA { get; }
+    }
+}
